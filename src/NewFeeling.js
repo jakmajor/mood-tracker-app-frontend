539ac5268fa813ff582a0dba1087cac8a24d91d0
Connor <alumitti@DESKTOP-13091IV.localdomain>
2021-10-27 09:34:01 -0700

    added state and stuff buttons

--- src/NewFeeling.js
+++ src/NewFeeling.js
@@ -1,7 +1,24 @@
+import React, {useState} from "react";
+
+
+
 function NewFeeling() {
+    const [selectedColor, setSelectedColor] = useState("");
+
+const handleClick = (e) => {
+
+}
+    
+    
+    
     return (
         <div>
-            <h1>hi from NewFeeling!</h1>
+            <button id="color_button_1" onClick={handleClick}>stuff1</button>
+            <button id="color_button_2" onClick={handleClick}>stuff2</button>
+            <button id="color_button_3" onClick={handleClick}>stuff3</button>
+            <button id="color_button_4" onClick={handleClick}>stuff4</button>
+            <button id="color_button_5" onClick={handleClick}>stuff5</button>
+            <button id="color_button_6" onClick={handleClick}>stuff6</button>
         </div>
     );
     }
