a427250bae3b8463dadd242be6579e2f55d529d0
Jak Major <66277064+jakmajor@users.noreply.github.com>
2021-10-27 11:28:57 -0700

    fixing some files

--- src/NewFeeling.js
+++ src/NewFeeling.js
@@ -1,9 +1,39 @@
-function NewFeeling() {
+
+import React, { useEffect, useState } from "react";
+function NewFeeling({ user }) {
+    const [selectedColor, setSelectedColor] = useState("");
+    const [text, setText] = useState("");
+
+    const handleClick = (e) => {
+        setSelectedColor(e.target.value)
+
+    }
+
+    const createFeelings = () => {
+        fetch(`http://localhost:9292/feelings/${user.id}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                color: selectedColor,
+                text: text
+            }),
+        })
+    }
+    console.log(createFeelings)
     return (
         <div>
-            <h1>hi from NewFeeling!</h1>
+            <button value="6A57F3" onClick={handleClick}>stuff1</button>
+            <button value="9B4CEF" onClick={handleClick}>stuff2</button>
+            <button value="E64786" onClick={handleClick}>stuff3</button>
+            <button value="E377AF" onClick={handleClick}>stuff4</button>
+            <button value="EC866B" onClick={handleClick}>stuff5</button>
+            <button value="FFCC47" onClick={handleClick}>stuff6</button>
+            <input onChange={(e) => setText(e.target.value)} />
+            <button onClick={(e) => createFeelings()}>Submit</button>
         </div>
     );
-    }
-    
-    export default NewFeeling;
+}
+
+export default NewFeeling;
