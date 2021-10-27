f7aa54fdac5db07a4c0c196c62e5828ced9c33d7
Connor Hollins <88451388+Alumitti@users.noreply.github.com>
2021-10-27 11:33:33 -0700

    Update NewFeeling.js

--- src/NewFeeling.js
+++ src/NewFeeling.js
@@ -1,24 +1,43 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 
-function NewFeeling() {
+function NewFeeling({user}) {
     const [selectedColor, setSelectedColor] = useState("");
+    const [text, setText] = useState("");
+    
+    const handleClick = (e) => {
+        setSelectedColor(e.target.value)
+        
+    }
+    
 
-const handleClick = (e) => {
+    const createFeelings = () => {
+    fetch(`http://localhost:9292/feelings/${user.id}`,{
+        method:"POST",
+        headers:{
+            "Content-Type" : "application/json"
+        },
+        body:JSON.stringify({
+            color: selectedColor,
+            text: text
+        }),
+    })
+    }
 
-}
-   
+
     return (
         <div>
-            <button id="color_button_1" onClick={handleClick}>stuff1</button>
-            <button id="color_button_2" onClick={handleClick}>stuff2</button>
-            <button id="color_button_3" onClick={handleClick}>stuff3</button>
-            <button id="color_button_4" onClick={handleClick}>stuff4</button>
-            <button id="color_button_5" onClick={handleClick}>stuff5</button>
-            <button id="color_button_6" onClick={handleClick}>stuff6</button>
+            <button value="6A57F3" onClick={handleClick}>stuff1</button>
+            <button value="9B4CEF" onClick={handleClick}>stuff2</button>
+            <button value="E64786" onClick={handleClick}>stuff3</button>
+            <button value="E377AF" onClick={handleClick}>stuff4</button>
+            <button value="EC866B" onClick={handleClick}>stuff5</button>
+            <button value="FFCC47" onClick={handleClick}>stuff6</button>
+            <input onChange={(e) => setText(e.target.value)} />
+            <button onClick={(e) => createFeelings()}>Submit</button>
         </div>
     );
-}
-
-export default NewFeeling;
+    }
+    
+    export default NewFeeling;
