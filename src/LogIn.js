1d884fc4616123d6e39ac2a5e6858de57a686b94
Connor Hollins <88451388+Alumitti@users.noreply.github.com>
2021-10-27 11:34:34 -0700

    Update LogIn.js

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-function LogIn({ setUser }) {
 
-
+function LogIn({setUser, user}) {
     const [name, setName] = useState("")
+
+
     const logIn = (data) => {
         let day = new Date(data.feelings[data.feelings.length - 1].created_at)
         let today = new Date()
@@ -14,26 +15,30 @@ function LogIn({ setUser }) {
         }
         // console.log(day)
     }
+
     const getFeelers = () => {
-        fetch(`http://localhost:9292/feelers/${name}`, {
+        fetch(`http://localhost:9292/feelers/${user}`, {
             headers: {
                 "Content-Type": "application/json",
             }
         })
-            .then(response => response.json())
-            .then(data => {
-                logIn(data);
-            })
+        .then(response => response.json())
+        .then(data => {
+            logIn(data);
+        })
     }
+
     return (
         <div className="frosty-box login-box">
             <h1 className="login-text">What is your name?</h1>
             <div className="login-input-wrapper">
-                <input onChange={(e) => setName(e.target.value)} placeholder="Enter your name..." />
+                <input onChange={(e) => setUser(e.target.value)} placeholder="Enter your name..." />
                 <button onClick={() => getFeelers()} className="button login-button">+</button>
             </div>
         </div>
     );
 }
+
 // a lot of stuff
+
 export default LogIn;
